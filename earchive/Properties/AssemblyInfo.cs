a9743c0ae0d4481bd169d025cd624db7c835f0f4
Andrey Gankov <gav@qsolution.ru>
2016-04-08 11:55:44 +0300

    Выпуск 0.8.10

--- earchive/Properties/AssemblyInfo.cs
+++ earchive/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using QSProjectsLib;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("QS: Электронный архив")]
-[assembly: AssemblyCopyright("Quality Solution 2015")]
+[assembly: AssemblyCopyright("Quality Solution 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyLogoIcon("earchive.icons.logo.png")]
@@ -24,7 +24,7 @@ using QSProjectsLib;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.8.9")]
+[assembly: AssemblyVersion("0.8.10")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
