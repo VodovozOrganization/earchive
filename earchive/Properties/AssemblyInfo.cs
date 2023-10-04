50986861604bb099c42a9b1fb61346fc81f80d61
Semen <nikiforov.semen@gmail.com>
2023-10-04 22:05:48 +0300

    Информация о сборке

--- earchive/Properties/AssemblyInfo.cs
+++ earchive/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using QS.Project.Versioning;
-using QSProjectsLib;
+using System.Reflection;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
@@ -19,13 +17,13 @@ using QSProjectsLib;
 [assembly: AssemblyAppWebsite("http://www.qsolution.ru/")]
 
 [assembly: AssemblyAuthor("Ганьков Андрей <gav@qsolution.ru>")]
-[assembly: AssemblyAuthor ("Гордеев Вадим 2018- <enzogord@gmail.com>")]
+[assembly: AssemblyAuthor("Гордеев Вадим 2018- <enzogord@gmail.com>")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.8.19")]
+[assembly: AssemblyVersion("0.8.20")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
